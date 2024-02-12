1dd6077ee0137d12f038885156f8e89897240fda
Welton <weltonrdesigner@gmail.com>
2024-02-12 15:08:54 -0300

    server3

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const port = 3000;
 
 // Escutar a porta 3000
 app.listen(port, () => {
-    console.log(`Server running on port http://localhost:${port}/`);
-});
+    console.log(`Server running on port http://localhost:${port}`)
+})
