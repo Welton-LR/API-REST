fdd6bfd148b9d6ae43484ed1662ea6e65642fd49
Welton <weltonrdesigner@gmail.com>
2024-02-14 21:22:06 -0300

    status200

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import express from 'express'
 const app = express()
 
+//mock
+const selecoes = [
+    {id:1, selecao: 'Brasil', grupo: 'G'},
+    {id:2, selecao: 'Suiça', grupo: 'G'},
+    {id:3, selecao: 'Sérvia', grupo: 'G'},
+    {id:4, selecao: 'Camarões', grupo: 'G'},
+]
 
 //criar rota  padrão ou raiz
 app.get('/', (req, res) => {
     res.send('Curso Node!')
 })
-  
+
+app.get('/selecoes', (req, res) =>{
+    res.status(200).send(selecoes)
+})
 export default app
 
 
