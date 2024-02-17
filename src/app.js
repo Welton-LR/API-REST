92482ed80723eb5da086c39bf6fcef9d478c79d8
Welton <weltonrdesigner@gmail.com>
2024-02-17 20:25:41 -0300

    Post com Insomnia

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express'
 const app = express()
 
+//Indica qie o Express precisa ler o corpo da requisição com Json
+app.use(express.json())
+
 //mock
 const selecoes = [
     {id:1, selecao: 'Brasil', grupo: 'G'},
@@ -14,9 +17,15 @@ app.get('/', (req, res) => {
     res.send('Curso Node!')
 })
 
-app.get('/selecoes', (req, res) =>{
+app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes)
 })
+
+app.post('/selecoes', (req, res) => {
+    selecoes.push(req.body)
+    res.status(201).send('Seleção cadastrada com sucesso!')
+})
+
 export default app
 
 
