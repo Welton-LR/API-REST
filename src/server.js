449135e3df16d90ca0a014f63844d2b908274f00
Welton <weltonrdesigner@gmail.com>
2024-02-25 20:52:59 -0300

    Conexao2

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 import app from './app.js'
 
+import conexao from '../infra/conexap.js'
+
 const PORT = 3000
 
-// Escutar a porta 3000
-app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`)
-})
+//Fazer conexão
+conexao.connect((erro) => {
+    if(erro) {
+        console.log(erro);
+    } else {
+        console.log("Conexão realizada com sucesso!");
+
+        // Escutar a porta 3000
+        app.listen(PORT, () => {
+            console.log(`Server running on port http://localhost:${PORT}`)
+        })
+          }
+    })    
